63b36afe74e5006aca3d582744426f7e75312eab
Marcian Diamond <marciandmnd@yahoo.com>
2017-12-11 17:14:30 -0800

    fix logout link assertion message

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -24,7 +24,7 @@ describe('<NavigationItems />', () => {
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
 
-  it('should render three <NavigationItem /> elements if user is authenticated', () => {
+  it('should render logout NavigationItem if user is authenticated', () => {
     wrapper.setProps({isAuthenticated: true});
     expect(wrapper.contains(<NavigationItem link='/logout'>Logout</NavigationItem>)).toEqual(true);
   });
